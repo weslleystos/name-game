7587d2d678995512fe136b402aaab7b7312bcaaf
Weslley Campos <weslley_campos@outlook.com>
2024-03-10 17:37:06 -0300

    Adding the colors theme.

--- app/src/main/java/com/github/weslleystos/namegame/presentation/theme/Theme.kt
+++ app/src/main/java/com/github/weslleystos/namegame/presentation/theme/Theme.kt
@@ -15,16 +15,16 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+//private val DarkColorScheme = darkColorScheme(
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+//)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = PrimaryColor,
+    secondary = SecondaryColor,
+    tertiary = TertiaryColor
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -50,7 +50,7 @@ fun NameGameTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
+//        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
